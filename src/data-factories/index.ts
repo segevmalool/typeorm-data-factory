c40f286a43572821c1bc31e3403bd667f1e81f93
Segev Malool <segevmalool@Segevs-MacBook-Pro.local>
2022-07-10 17:25:08 -0700

    working data gen

--- src/data-factories/index.ts
+++ src/data-factories/index.ts
@@ -3,62 +3,75 @@ import { GlobalDataSource } from '../typeorm';
 import { ColumnType, DataSource, EntityMetadata, ObjectLiteral } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
-// The reason for the string key is to handle the constructor-based ColumnTypes.
+// The string key is to handle the constructor-based ColumnTypes.
 const typeFactory: { [key: string]: Function } = {
   uuid,
   string: () => 'bob',
   'function String() { [native code] }': () => 'bob',
+  numeric: () => 35.00
 };
 
-export function generateInstanceData(entityMeta: EntityMetadata) {
-  // Generates a record for an arbitrary db entity.
+function generateInstanceData(entityMeta: EntityMetadata): ObjectLiteral[] {
 
-  const instanceData: typeof entityMeta.propertiesMap = {};
+  function _generateInstanceData(entityMeta: EntityMetadata, accumulator: ObjectLiteral[]): ObjectLiteral[] {
+    // Generates records for an arbitrary db entity and its dependencies.
+    let dependenciesInstanceData: ObjectLiteral;
+    const instanceData: typeof entityMeta.propertiesMap = {};
 
-  for (let colMeta of entityMeta.columns) {
-    if (colMeta.isGenerated) {
-      continue;
+    for (let colMeta of entityMeta.columns) {
+      if (colMeta.referencedColumn) {
+        dependenciesInstanceData = _generateInstanceData(colMeta.referencedColumn.entityMetadata, accumulator);
+      }
+
+      // Generate a data for the column
+      const colType = colMeta.type as ColumnType;
+      const colTypeFactory = typeFactory[colType.toString()];
+
+      if (!colTypeFactory) {
+        // if no factory is defined for this type, return null
+        instanceData[colMeta.propertyName] = null;
+      } else {
+        instanceData[colMeta.propertyName] = colTypeFactory();
+      }
+
+      if (colMeta.referencedColumn) {
+        // if this is a foreign key reference, override the generated id with the dependency id
+        // @ts-ignore
+        instanceData[colMeta.propertyName] = dependenciesInstanceData[dependenciesInstanceData.length - 1].id;
+      }
     }
 
-    const colType = colMeta.type as ColumnType;
-    const colTypeFactory = typeFactory[colType.toString()];
-
-    if (!colTypeFactory) {
-      // if no factory is defined for this type, return null
-      instanceData[colMeta.propertyName] = null;
-    } else {
-      instanceData[colMeta.propertyName] = colTypeFactory();
-    }
+    accumulator.push(instanceData);
+    return [...accumulator.filter(entity => entity.id !== instanceData.id), instanceData];
   }
 
-  return instanceData;
+  return _generateInstanceData(entityMeta, []);
 }
 
 function generateSingleEntity<T extends EntityMetadata>(
-    entityMeta: T,
-    instanceData: typeof EntityMetadata.prototype.propertiesMap,
-    dataSource: DataSource
+  entityMeta: T,
+  instanceData: typeof EntityMetadata.prototype.propertiesMap,
+  dataSource: DataSource
 ) {
-    const instance = dataSource.manager.create(
-        entityMeta.inheritanceTree[0].prototype.constructor,
-        instanceData
-    );
+  const instance = dataSource.manager.create(
+    entityMeta.inheritanceTree[0].prototype.constructor,
+    instanceData
+  );
 
-    return instance;
+  return instance;
 }
 
 function generateAllEntities(dataSource: DataSource) {
-    const allEntities: ObjectLiteral[] = [];
+  // Generates records for all entities in the datasource, respecting foreign keys.
+  const rootEntity = dataSource.entityMetadatas[dataSource.entityMetadatas.length-1];
 
-    for (let entityMeta of dataSource.entityMetadatas) {
-        const instanceData: typeof entityMeta.propertiesMap = generateInstanceData(entityMeta);
+  const instanceData = generateInstanceData(rootEntity);
+  const allEntities = instanceData.map(
+      (instanceData) => generateSingleEntity(rootEntity, instanceData, dataSource)
+  );
+  console.log(allEntities);
 
-        const entity = generateSingleEntity(entityMeta, instanceData, dataSource);
-
-        allEntities.push(entity);
-    }
-
-    return allEntities;
+  return allEntities;
 }
 
 async function main() {
--- src/entities/transaction.entity.ts
+++ src/entities/transaction.entity.ts
@@ -1,7 +1,17 @@
-import { Entity, PrimaryColumn } from 'typeorm';
+import {Column, Entity, ManyToOne, PrimaryColumn} from 'typeorm';
+import { User } from './user.entity';
 
 @Entity({ name: 'transactions' })
 export class Transaction {
   @PrimaryColumn({ type: 'uuid' })
   id: string;
+
+  @ManyToOne((type) => User, (user) => user.id)
+  from_user: User;
+
+  @ManyToOne((type) => User, (user) => user.id)
+  to_user: User;
+
+  @Column({ type: 'numeric'})
+  amount: number | string;
 }
