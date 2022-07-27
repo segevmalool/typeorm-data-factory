cee4c6b0ecf4cd6e9d5c34a379c18a1e9596cf78
Segev Malool <segevmalool@Segevs-MacBook-Pro.local>
2022-07-26 19:54:19 -0700

    lift to records and respect non-unique fks

--- src/data-factory/index.ts
+++ src/data-factory/index.ts
@@ -1,6 +1,8 @@
+import assert from 'assert';
 import {
   ColumnType, DataSource, EntityManager, EntityMetadata, ObjectLiteral, Entity
 } from 'typeorm';
+import { ColumnMetadata } from 'typeorm/metadata/ColumnMetadata';
 import { v4 as uuid } from 'uuid';
 
 // The string key is to handle the constructor-based ColumnTypes.
@@ -16,42 +18,126 @@ export interface DataWithDescription {
   instanceData: ObjectLiteral;
 }
 
+const PLACEHOLDER = uuid();
+
 export function generateInstanceDataWithDependencies(
   entityMeta: EntityMetadata
 ): DataWithDescription[] {
-  function _generateInstanceData(
-    entityMeta: EntityMetadata,
-    accumulator: DataWithDescription[]
-  ): DataWithDescription[] {
-    // Generates records for an arbitrary db entity and its dependencies.
-    let dependenciesInstanceData: DataWithDescription[] = [];
+
+  function _generateInstanceWithRelationshipPlaceholders<T extends EntityMetadata>(
+      entityMeta: T
+  ): T['propertiesMap'] {
     const instanceData: typeof entityMeta.propertiesMap = {};
 
     for (let colMeta of entityMeta.columns) {
-      if (colMeta.referencedColumn) {
-        dependenciesInstanceData = _generateInstanceData(
-          colMeta.referencedColumn.entityMetadata,
-          accumulator
-        );
-      }
+      let generatedValue = null;
 
-      // Generate a data for the column
-      const colType = colMeta.type as ColumnType;
-      const colTypeFactory = typeFactory[colType.toString()];
-      let generatedValue = colTypeFactory() || null;
-
-      if (colMeta.referencedColumn) {
-        // if this is a foreign key reference, override the generated id with the dependency id
-        generatedValue = dependenciesInstanceData[dependenciesInstanceData.length - 1].instanceData.id;
+      if (colMeta.referencedColumn && colMeta.relationMetadata) {
+        // Log the column name so we can generate a dependency later.
+        generatedValue = PLACEHOLDER;
+      } else {
+        // Generate a data for the column
+        const colType = colMeta.type as ColumnType;
+        const colTypeFactory = typeFactory[colType.toString()];
+        generatedValue = colTypeFactory() || null;
       }
 
       instanceData[colMeta.propertyName] = generatedValue;
     }
 
-    accumulator.push({
-      instanceData,
-      entity: entityMeta,
-    } as DataWithDescription);
+    return instanceData;
+  }
+
+  function getRelationshipColumns(entityMeta: EntityMetadata) {
+    const dependencyColumns = []
+    for (let colMeta of entityMeta.columns) {
+      if (colMeta.referencedColumn && colMeta.relationMetadata) {
+        dependencyColumns.push(colMeta);
+      }
+    }
+    return dependencyColumns;
+  }
+
+  function generateOneOrManyInstances(
+      dependencyColumns: ColumnMetadata[],
+      entityMeta: EntityMetadata
+  ): {[key: string]: (typeof entityMeta.propertiesMap)[]} {
+    const instances: {[key: string]: (typeof entityMeta.propertiesMap)[]} = {};
+    for (const colMeta of dependencyColumns) {
+      assert(
+          colMeta.relationMetadata && colMeta.referencedColumn,
+          "Column is not an FK (2)."
+      );
+
+      const numInstances = colMeta.relationMetadata.isOneToOne ? 1 : 3;
+
+      for (let i = 0; i < numInstances; i += 1) {
+        const instance = _generateInstanceWithRelationshipPlaceholders(entityMeta);
+        if (!instances[colMeta.propertyName])
+          instances[colMeta.propertyName] = []
+        instances[colMeta.propertyName].push(instance);
+      }
+    }
+
+    return instances;
+  }
+
+  function _generateInstanceData(
+    entityMeta: EntityMetadata,
+    accumulator: DataWithDescription[]
+  ): DataWithDescription[] {
+    let allDependenciesInstanceData: DataWithDescription[] = [];
+
+    // 1. Find FK cols.
+    const dependencyColumns: ColumnMetadata[] =
+        getRelationshipColumns(entityMeta);
+
+    // 2. Generate one or many placeholder records for each dependency column
+    const instances: {[key: string]: (typeof entityMeta.propertiesMap)[]} =
+        generateOneOrManyInstances(dependencyColumns, entityMeta);
+
+    // 3. Generate dependencies for fk columns and fill in the
+    const visitedDependencies: {[key: string]: DataWithDescription[]} = {};
+    for (const colMeta of dependencyColumns) {
+      assert(
+          colMeta.relationMetadata && colMeta.referencedColumn,
+          "Column is not an FK (3)."
+      );
+
+      visitedDependencies[colMeta.propertyName] = _generateInstanceData(
+          colMeta.referencedColumn.entityMetadata,
+          accumulator
+      );
+
+      for (const colMetaTwo of dependencyColumns) {
+        for (const instance of instances[colMetaTwo.propertyName]) {
+          for (const colName in visitedDependencies) {
+            if (instance[colName] !== PLACEHOLDER) continue;
+            const dependency = visitedDependencies[colName];
+            instance[colName] =
+                dependency[dependency.length - 1].instanceData.id;
+          }
+        }
+      }
+    }
+
+    for (const colMeta of dependencyColumns) {
+      accumulator.push(
+          ...instances[colMeta.propertyName].map((instance) => ({
+            instanceData: instance,
+            entity: entityMeta,
+          } as DataWithDescription))
+      );
+    }
+
+    // 4. If it's a root, then generate a single instance.
+    if (dependencyColumns.length === 0) {
+      const instance = _generateInstanceWithRelationshipPlaceholders(entityMeta);
+      accumulator.push({
+        instanceData: instance,
+        entity: entityMeta,
+      } as DataWithDescription);
+    }
 
     return accumulator;
   }
