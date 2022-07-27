c1cb74af4243548ccab8dc37960b7f3fa32f242e
Segev Malool <segevmalool@Segevs-MacBook-Pro.local>
2022-07-26 20:15:01 -0700

    refactor -- refine data generator and functional test

---
+++ test/index.ts
@@ -0,0 +1,23 @@
+import { GlobalDataSource } from './typeorm';
+import { ObjectLiteral } from 'typeorm';
+import { generateEntitiesWithDependencies } from '../src/data-factory';
+
+async function main() {
+  // Assume the db is built up as needed for seeding.
+  await GlobalDataSource.initialize();
+
+  const allEntities: ObjectLiteral[] = generateEntitiesWithDependencies(
+      GlobalDataSource.entityMetadatas[GlobalDataSource.entityMetadatas.length - 1],
+      GlobalDataSource.manager
+  );
+
+  await GlobalDataSource.manager.save(allEntities);
+
+  await GlobalDataSource.destroy();
+
+  return allEntities;
+}
+
+if (require.main === module) {
+  main().then((entities) => console.log('Created entities: ', entities));
+}
